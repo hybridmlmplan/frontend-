f4afbdd36aeb2dbb296b88c538a9fc877f5001ba
hybridmlmplan <osfdoi@gmail.com>
2025-11-26 11:10:31 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,19 +1,17 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
-export const loginUser = async (email, password) => {
-  try {
-    const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Login API Error:", error);
-    return { success: false, message: "Server error!" };
-  }
-};
+export default App;
