9f91b386dfdd11462b28f086126bb1434b3528d8
hybridmlmplan <osfdoi@gmail.com>
2025-11-28 09:18:10 +0530

    Update api.js

--- src/src/api.js
+++ src/src/api.js
@@ -1,13 +1,26 @@
 // src/api.js
-const API_BASE_URL = "https://backend-1lby.onrender.com"; // Your Render backend URL
 
-export async function loginUser(username, password) {
+const API_BASE_URL = "https://mlmplan-backend.onrender.com/api/auth"; 
+
+export async function loginUser(email, password) {
   const response = await fetch(`${API_BASE_URL}/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ email, password }),
+  });
+
+  return response.json();
+}
+
+export async function register(formData) {
+  const response = await fetch(`${API_BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
   });
 
   return response.json();
