5d4f232a39464bac2f828e5810cbf543c357636f
hybridmlmplan <osfdoi@gmail.com>
2025-11-28 15:13:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  preview: {
-    allowedHosts: ["mlmplan-frontend-43ru.onrender.com"],
-  }
+  plugins: [react()],
 });
