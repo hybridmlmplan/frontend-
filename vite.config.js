ef7daf9f116abecf56d09eb8a83cdcecac76f75c
hybridmlmplan <osfdoi@gmail.com>
2025-11-29 16:36:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,23 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://backend-1-1b8h.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
+
+  build: {
+    outDir: "dist",
+  },
+
   preview: {
-    port: 10000
+    port: 10000,
   }
 });
