4e92f577b324198b906c3859bcd25963c865f5a5
hybridmlmplan <osfdoi@gmail.com>
2025-12-01 16:17:22 +0530

    Create KycUpload.jsx

---
+++ src/pages/settings/KycUpload.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function KycUpload() {
+  return (
+    <div className="container card">
+      <h2>Upload KYC</h2>
+      <input type="file" />
+      <button>Upload</button>
+    </div>
+  );
+}
