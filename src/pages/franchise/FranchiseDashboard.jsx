f1e48a7b25a90575566dbd5a5e06165af7c67a47
hybridmlmplan <osfdoi@gmail.com>
2025-12-01 15:39:54 +0530

    Create FranchiseDashboard.jsx

---
+++ src/pages/franchise/FranchiseDashboard.jsx
@@ -0,0 +1,8 @@
+export default function FranchiseDashboard() {
+  return (
+    <div className="page">
+      <h2>Franchise Dashboard</h2>
+      <p>Welcome Franchise Partner</p>
+    </div>
+  );
+}
