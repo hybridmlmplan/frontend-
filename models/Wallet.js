265b69af1a564c46478102e3dffaad6549b17b07
hybridmlmplan <osfdoi@gmail.com>
2025-12-01 16:57:18 +0530

    Update Wallet.js

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose');
 
-const WalletSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
-  balance: { type: Number, default: 0 },
-  pending: { type: Number, default: 0 },
-  creditHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'BVHistory' }], // refs
-  debitHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'BVHistory' }],
-  updatedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Wallet', WalletSchema);
