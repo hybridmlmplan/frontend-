c9f8d48a8caaccf9cbd429ec848685f819932eee
hybridmlmplan <osfdoi@gmail.com>
2025-12-01 17:02:32 +0530

    Update package.js

--- routes/package.js
+++ routes/package.js
@@ -1,15 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const packageController = require('../controllers/packageController');
-const authMiddleware = require('../middleware/authMiddleware');
 
-// public: list packages
-router.get('/', packageController.listPackages);
-
-// admin: create package (optional middleware check)
-router.post('/', authMiddleware.verifyAdmin, packageController.createPackage);
-
-// get package by id
-router.get('/:id', packageController.getPackage);
-
-module.exports = router;
