365b493fee8a8d6c3f40bc317584322dae1c3663
hybridmlmplan <osfdoi@gmail.com>
2025-12-01 16:20:15 +0530

    Create HomeService.jsx

---
+++ src/pages/services/HomeService.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomeService() {
+  return (
+    <div className="container card">
+      <h2>Home Services</h2>
+      <p>Plumbing, Electrician, Cleaning service integrations</p>
+    </div>
+  );
+}
