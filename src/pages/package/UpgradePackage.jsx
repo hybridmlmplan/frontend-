2399566274ab7b414389c827dadca60df88d5e81
hybridmlmplan <osfdoi@gmail.com>
2025-12-09 22:06:04 +0530

    Create UpgradePackage.jsx

---
+++ src/pages/package/UpgradePackage.jsx
@@ -0,0 +1,2 @@
+import React from "react";
+export default function UpgradePackage(){ return <div className="p-4 bg-white rounded shadow">Upgrade package flow — use BuyPackage component to select higher package and pay/epin.</div>; }
