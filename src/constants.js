83eb4a5ca8248f77d154af31186677562ebe4a83
hybridmlmplan <osfdoi@gmail.com>
2025-12-09 18:13:00 +0530

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const PACKAGES = {
+  silver: { code: "silver", name: "Silver", price: 35, pv: 35 },
+  gold: { code: "gold", name: "Gold", price: 155, pv: 155 },
+  ruby: { code: "ruby", name: "Ruby", price: 1250, pv: 1250 }
+};
